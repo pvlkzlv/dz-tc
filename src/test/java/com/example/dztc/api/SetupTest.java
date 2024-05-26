b7a265ad281fd4bf7b4268d699d4b107460adda3
Pavel Kozlov <pavel.kazlou5440@gmail.com>
2024-05-26 18:12:40 +0400

    Is this it? Finally? PleASE Plsssss

--- src/test/java/com/example/dztc/api/SetupTest.java
+++ src/test/java/com/example/dztc/api/SetupTest.java
@@ -3,9 +3,10 @@ package com.example.dztc.api;
 import org.testng.annotations.BeforeSuite;
 
 public class SetupTest extends BaseApiTest {
+
     @BeforeSuite(alwaysRun = true)
     public void setAuthSettings() {
-        //TODO figure out the way to avoid blank strings
+        System.out.println("Setting up auth settings and debug");
         var authSettings = checkedWithSuperUser.getAuthSettings().get("");
         authSettings.setPerProjectPermissions(true);
         checkedWithSuperUser.getAuthSettings().update(authSettings);
